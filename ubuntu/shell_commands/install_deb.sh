9597b0be9cabed055df8929dacb22ca784b338d9
cuongpiger <cuongpigerr@gmail.com>
2023-08-10 09:05:12 +0700

    add script to install chrome browser

--- ubuntu/shell_commands/install_deb.sh
+++ ubuntu/shell_commands/install_deb.sh
@@ -25,7 +25,5 @@ sudo apt install wget -y
 # install Chrome browser
 chrome_dest="$HOME/Downloads/google-chrome-stable_current_amd64.deb"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O $chrome_dest
-
 sudo apt update && sudo apt install $chrome_dest -y
-
 echo "[INFO] Chrome browser is installed successfully!"
