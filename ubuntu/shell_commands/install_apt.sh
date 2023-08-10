9af7927c6068e79e9eef8b4baa52222f684e0aa5
cuongpiger <cuongpigerr@gmail.com>
2023-08-10 10:36:04 +0700

    This is Manh Cuong's commit 🌻.

---
+++ ubuntu/shell_commands/install_apt.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+####################################################################################################
+# Author: Cuong. Duong Manh
+# Date: 2023/08/10
+#
+# Description: Install some apps using apt.
+# OS: Ubuntu 22.04 LTS
+####################################################################################################
+
+sudo_pw=$1
+
+if [ -z "$sudo_pw" ]
+then
+  echo "Please provide sudo password!"
+  exit 1
+fi
+
+# make sure update the OS before installing something apps
+echo "$sudo_pw" | sudo -S apt update
+
+sudo apt-get -y install libfuse2
+
