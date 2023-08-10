7d3d005b2b4b5a544a40448d3c9f76f23f58716f
cuongpiger <cuongpigerr@gmail.com>
2023-08-10 11:47:11 +0700

    This is Manh Cuong's commit 🌻.

---
+++ ubuntu/shell_commands/install_snap_app.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo_pw=$1
+
+if [ -z "$sudo_pw" ]
+then
+  echo "Please provide sudo password!"
+  exit 1
+fi
+
+sudo_pw=$1
+
+if [ -z "$sudo_pw" ]
+then
+  echo "Please provide sudo password!"
+  exit 1
+fi
+
+# make sure update the OS before installing something apps
+echo "$sudo_pw" | sudo -S apt update
+
+sudo snap install slack telegram-desktop skype teams-for-linux
--- ubuntu/shell_commands/setup_git.sh
+++ ubuntu/shell_commands/setup_git.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 ####################################################################################################
 # Author: Cuong. Duong Manh
@@ -14,8 +14,7 @@
 
 sudo_pw=$1
 
-if [ -z "$sudo_pw" ]
-then
+if [ -z "$sudo_pw" ]; then
   echo "Please provide sudo password!"
   exit 1
 fi
@@ -31,13 +30,13 @@ public_key="$HOME/.ssh/personal_git.pub"
 zshrc_file="$HOME/.zshrc"
 
 if [ ! -f "$private_key" ]; then
-    echo "CRITICAL: $private_key does not exist!"
-    exit 1
+  echo "CRITICAL: $private_key does not exist!"
+  exit 1
 fi
 
 if [ ! -f "$public_key" ]; then
-    echo "CRITICAL: $public_key does not exist!"
-    exit 1
+  echo "CRITICAL: $public_key does not exist!"
+  exit 1
 fi
 
 chmod 0400 "$private_key"
@@ -48,21 +47,23 @@ chown "$USER:$USER" "$public_key"
 
 # alias git command
 if [ ! -f "$zshrc_file" ]; then
-    echo "CRITICAL: $zshrc_file does not exist!"
-    exit 1
+  echo "CRITICAL: $zshrc_file does not exist!"
+  exit 1
 fi
 
 cat >>"$zshrc_file" <<EOF
 function gs() {
-    # cmm=${1:-"This is my commit"}
-    cmm="$1"
-    if test -z "$cmm"
-    then
-        cmm="This is Manh Cuong's commit 🌻."
-    fi
+  # cmm=\${1:-"This is my commit"}
+  cmm="\$1"
+  if test -z "$cmm"
+  then
+    cmm="This is Manh Cuong's commit 🌻."
+  fi
     
-    git add .
-    git commit -m "$cmm"
-    git push
+  git add .
+  git commit -m "$cmm"
+  git push
 }
 EOF
+
+source $HOME/.zshrc
