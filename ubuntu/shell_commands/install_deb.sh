a767e960f73790450dae2d0d247b609c371c9f0d
cuongpiger <cuongpigerr@gmail.com>
2023-08-10 09:07:30 +0700

    [chore] update the gitignore

--- ubuntu/shell_commands/install_deb.sh
+++ ubuntu/shell_commands/install_deb.sh
@@ -25,5 +25,5 @@ sudo apt install wget -y
 # install Chrome browser
 chrome_dest="$HOME/Downloads/google-chrome-stable_current_amd64.deb"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O $chrome_dest
-sudo apt update && sudo apt install $chrome_dest -y
+sudo apt update && sudo apt install $chrome_dest -y && rm -rf $chrome_dest
 echo "[INFO] Chrome browser is installed successfully!"
