4af5e1a86e7610d07991170bd9950c493cfbd9f1
cuongpiger <cuongpigerr@gmail.com>
2023-08-12 12:37:25 +0700

    This is Manh Cuong's commit 🌻.

--- ubuntu/shell_commands/05_config_desktop.sh
+++ ubuntu/shell_commands/05_config_desktop.sh
@@ -11,3 +11,9 @@
 # change the text-scaling-factor to 0.76 and move the window buttons to the left
 gsettings set org.gnome.desktop.interface text-scaling-factor 0.76
 gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:'
+
+# fix bug evince not open browser when click on link
+sudo ln -s /etc/apparmor.d/usr.bin.evince /etc/apparmor.d/disable/ 
+sudo apparmor_parser -R /etc/apparmor.d/disable/usr.bin.evince
+
+echo "DONE! 05_config_desktop.sh"
